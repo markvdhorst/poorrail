2a96feb4e9f1aa8024cab341537ea88e046600eb
Mark van der Horst <markvdhorst0@gmail.com>
2017-12-20 11:25:14 +0100

    Controller methods and main structure
    Added methods from traincompany to controller.
    Added basic structure for initializing the application.

--- src/main/java/nl/hu/pafr/main/Main.java
+++ src/main/java/nl/hu/pafr/main/Main.java
@@ -1,9 +1,18 @@
 package nl.hu.pafr.main;
 
 import nl.hu.pafr.controller.TrainCompanyController;
+import nl.hu.pafr.model.TrainCompany;
+import nl.hu.pafr.view.GraphicalTrainCompanyView;
 
 public class Main {
 	public static void main(String[] args){
-		TrainCompanyController controller = new TrainCompanyController();
+		//Hieronder is de basisstructuur van de code om de applicatie te starten. Deze code moet nog uitgebreid worden voordat het nuttig is.
+		TrainCompany trainCompany = loadTrainCompany();
+		TrainCompanyController controller = new TrainCompanyController(trainCompany);
+		GraphicalTrainCompanyView view = new GraphicalTrainCompanyView(controller);
+	}
+	//TODO: echte methode maken voor het inladen van een traincompany, mogelijk generiek maken/gebruik laten maken van interface
+	private static TrainCompany loadTrainCompany() {
+		return new TrainCompany();
 	}
 }
